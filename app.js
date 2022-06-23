88e950b1e3d44aff24d2a98e7da9cdae56876151
Ruben Cordero <rubencordero01@gmail.com>
2022-06-23 12:49:04 -0500

    Fixes formatting on timer

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
 
 const app = express();
 
@@ -13,15 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  let day = date.getDate();
   //render looks for a list inside of the views folder and replaces the kindOfDay with whatever matches the day
   res.render("list", { listTitle: day, newListItems: items });
 });
